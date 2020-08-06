2be2bebd8dfac2d0e53aaa32eda750fb34ce0ae7
hunterakins <yunterakins@gmail.com>
2020-08-06 11:37:32 -0700

    garbage works, but you may have to tweak the files that get gammas from the filenames because I use inconsistent suffixes in my processing...sorry. I think extract_modes.py will replace garbage.py and will be a better thing in general

--- audio/garbage.py
+++ audio/garbage.py
@@ -18,26 +18,39 @@ Author: Hunter Akins
 '''
 
 
-def get_names(freq, chunk_len=1024,short=False):
+def get_names(freq, chunk_len=1024,short=False, chunk=None):
+    """
+    Look through the npy files directory for the saved files
+    """
     if short == True:
         names = [x for x in os.listdir(os.getcwd() + '/npy_files/' + str(freq)+ '/'+ 'short') if x[-5] != 'd']
+    elif type(chunk) != type(None):
+        names = [x for x in os.listdir(os.getcwd() + '/npy_files/' + str(freq)+ '/'+ 'chunk'+str(chunk)) if x[-5] != 'd']
+        
     else:
         names = [x for x in os.listdir(os.getcwd() + '/npy_files/' + str(freq)+ '/'+ str(chunk_len)) if x[-5] != 'd']
     return names
 
-def get_gammas_from_names(names,short=False):
+def get_gammas_from_names(names,short=False, chunk=None):
     if short == True:
         gammas = [float(x[:-15]) for x in names]
+    elif type(chunk) != type(None):
+        gammas = [float(x[:-7]) for x in names]
+        #gammas = [float(x[:-17]) for x in names]
     else:
-        gammas = [float(x[:-9]) for x in names]
+        gammas = [float(x[:-7]) for x in names]
     gammas.sort()
     return gammas
     
-def get_freqs(freq):
-    f = np.load('npy_files/' + str(freq) + '/f_grid.npy')
+def get_freqs(freq,chunk=None):
+    if type(chunk)==type(None):
+        f = np.load('npy_files/' + str(freq) + '/f_grid.npy')
+    else:
+        #f = np.load('npy_files/' + str(freq) + '/chunk' + str(chunk) + '/chunk_' + str(chunk) + '_f_grid.npy') 
+        f = np.load('npy_files/' + str(freq) + '/chunk' + str(chunk) + '/nb_f_grid.npy') 
     return f
 
-def get_dats_for_gamma(gamma, freq, chunk_len=1024,short=False):
+def get_dats_for_gamma(gamma, freq, chunk_len=1024,short=False,chunk=None):
     """
     For a value of gamma and frequency freq
     get the search over f provided
@@ -48,12 +61,17 @@ def get_dats_for_gamma(gamma, freq, chunk_len=1024,short=False):
     """
     if short == True:
         tail = '_short_' + str(chunk_len) + '.npy'
+    elif type(chunk) != type(None):
+        #tail = '_chunk_' + str(chunk) + '_' + str(chunk_len) + '.npy'
+        tail = '_nb.npy'
     else:
         tail = '_' + str(chunk_len) + '.npy'
     fname =  str(gamma)[:7]+ tail
     try:
         if short == True:
             x = np.load('npy_files/' +str(freq) + '/short/' + fname)
+        elif type(chunk) != type(None):
+            x = np.load('npy_files/' +str(freq) + '/chunk' + str(chunk) + '/' + fname)
         else:
             x = np.load('npy_files/' +str(freq) + '/' + str(chunk_len) + '/' + fname)
     except FileNotFoundError:
@@ -61,6 +79,8 @@ def get_dats_for_gamma(gamma, freq, chunk_len=1024,short=False):
             fname = str(gamma)[:7] + '000' + tail
             if short == True:
                 x = np.load('npy_files/' +str(freq) + '/short/' + fname)
+            elif type(chunk) != type(None):
+                x = np.load('npy_files/' +str(freq) + '/chunk' + str(chunk) + '/' + fname)
             else:
                 x = np.load('npy_files/' +str(freq) + '/' + str(chunk_len) + '/' + fname)
         except FileNotFoundError:
@@ -68,12 +88,16 @@ def get_dats_for_gamma(gamma, freq, chunk_len=1024,short=False):
                 fname = str(gamma)[:7] + '0' + tail
                 if short == True:
                     x = np.load('npy_files/' +str(freq) + '/short/' + fname)
+                elif type(chunk) != type(None):
+                    x = np.load('npy_files/' +str(freq) + '/chunk' + str(chunk) + '/' + fname)
                 else:
                     x = np.load('npy_files/' +str(freq) + '/' + str(chunk_len) + '/' + fname)
             except FileNotFoundError:
                 fname = str(gamma)[:7] + '00' + tail
                 if short == True:
                     x = np.load('npy_files/' +str(freq) + '/short/' + fname)
+                elif type(chunk) != type(None):
+                    x = np.load('npy_files/' +str(freq) + '/chunk' + str(chunk) + '/' + fname)
                 else:
                     x = np.load('npy_files/' +str(freq) + '/' + str(chunk_len) + '/' + fname)
     return x
@@ -83,53 +107,138 @@ def get_depths():
     depths = np.delete(depths, 21)
     return depths
     
-def gen_figs(freq, chunk_len=1024, short=False):
+def gen_figs(freq, chunk_len=1024, short=False, chunk=None):
     """ 
     Process all the shit for a given frequency 
     """
-    names = get_names(freq,short=short)
-    gammas = get_gammas_from_names(names,short)
+    krs = np.load('npy_files/' + str(freq) + '_krs.npy')
+    krs = krs.real
+    names = get_names(freq,short=short,chunk=chunk)
+    gammas = get_gammas_from_names(names,short,chunk=chunk)
     depths=  get_depths()
-    f = get_freqs(freq)
+    f = get_freqs(freq,chunk=chunk)
+    mean_f = np.mean(f)
+    mean_v = (np.mean(f) - freq)*1450/freq
+    print(mean_v)
+    kr_ests = mean_f+(krs-np.mean(krs))*mean_v/2/np.pi
+
+    ylim = 0
     for i, gamma in enumerate(gammas):
         fname = str(gamma)[:7]+'.npy'
-        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short)
+        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short, chunk=chunk)
         power = np.square(abs(x))
-        if i == 0:
-            psums = np.zeros((len(names), x.shape[0]))
-            powers = np.zeros(power.shape)
+        incoh_sum = np.sum(power, axis=1)
+        max_val = np.max(incoh_sum)
+        if max_val > ylim:
+            ylim = max_val
+    ylim *= 1.1
+
+    
+
+    for i, gamma in enumerate(gammas):
+        fname = str(gamma)[:7]+'.npy'
+        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short, chunk=chunk)
+        power = np.square(abs(x))
 
         fig, (ax1, ax2)=plt.subplots(2,1,sharex='col')
+        ax2.set_ylim([0, ylim])
         levels = np.linspace(np.min(power), np.max(power), 20)
-        ax1.contour(f, depths, power.T, levels=levels)
+        ax1.contour(f, depths[::-1], power.T, levels=levels)
+        incoh_sum = np.sum(power, axis=1)
+        peak_ind = np.argmax(incoh_sum)
+        peak_mode = x[peak_ind,:]
+        peak_mode.real /= np.max(abs(peak_mode.real))*.1*np.max(f)
+        peak_mode.imag /= np.max(abs(peak_mode.imag))*.1*np.max(f)
+        f_offset = f[int(len(f)*.75)]
+        peak_mode.real += f_offset
+        peak_mode.imag += f_offset
+        #ax1.plot(peak_mode.real, depths)
+        #ax1.plot(peak_mode.imag, depths)
         ax2.plot(f, np.sum(power, axis=1))
-        fig_name = 'pics/gammapows/' + str(freq) + '/' + str(i).zfill(3) + '.png'
-        plt.suptitle('Gamma = ' +str(gamma))
+        ax2.scatter(kr_ests, [ylim/10]*kr_ests.size, marker='x',color='k')
+        if type(chunk) != type(None):
+            fig_name = 'pics/gammapows/' + str(freq) + '/' +str(chunk) +'/'+ str(i).zfill(3) + '.png'
+        else:
+            fig_name = 'pics/gammapows/' + str(freq) + '/' + str(i).zfill(3) + '.png'
+        plt.suptitle('Gamma = ' +str(gamma) +',  Chunk ' + str(chunk))
         plt.xlabel('Frequency (Hz)')
         ax1.set_ylabel('Depth')
         ax2.set_ylabel('Power')
+        plt.show()
+        plt.savefig(fig_name)
+        plt.show()
+        plt.close(fig)
+
+def gen_range_dep_movie(freq, chunks, chunk_len=1024, short=False):
+    """
+    I'm looking into the range dependence of the waveguide
+    I perform my analysis (searching over gamma) for each 
+    interval in chunks (on the supercomputer)
+    Then I want to look at the evolution of the spectrum as the 
+    intervals change    
+    SO just stack a bunch of subplots
+    Input 
+    freq - int 
+    chunks - list of lists (the chunks in minutes)
+    chunk_len - int
+    short - bool
+    """
+    #track_chunks = [[0, 15],[5,20], [10, 25],[15,30], [20,35],[25,40],[30, 45],[35,50],[40,55]]
+    rep_chunk = chunks[0]
+    names = get_names(freq,short=short,chunk=0)
+    gammas = get_gammas_from_names(names,short,chunk=0)
+    num_chunks = len(chunks)
+
+    """ Get axis limits """
+    smallest ,biggest=1e9,0
+    for i in range(num_chunks):
+        f_grid = get_freqs(freq,chunk=i)
+        small, big = f_grid[0], f_grid[-1]
+        if small < smallest:
+            smallest = small
+        if big > biggest:
+            biggest = big
+
+    """ Loop through and create a figure for each gamma """
+    for i, gamma in enumerate(gammas):
+        fig, axes = plt.subplots(num_chunks, 1)
+        for j in range(num_chunks):
+            axis = axes[j]
+            axis.set_xlim([smallest, biggest])
+            try:
+                dats = get_dats_for_gamma(gamma, freq, chunk=j)
+            except:
+                dats = np.zeros(dats.shape)
+                pass
+            f_grid = get_freqs(freq,chunk=j)
+            mean_f = np.mean(f_grid)
+            avg_vel = (mean_f/freq-1)*1500
+            axis.text(f_grid[0], dats[0,0], str(avg_vel)[:4])
+            axis.scatter(mean_f, 0, color='r')
+            power = np.sum(np.square(abs(dats)), axis=1)
+            axis.plot(f_grid, power)
+        if 'chunks' not in os.listdir('pics/'+str(freq)):
+            os.system('mkdir pics/' + str(freq) +'/chunks')
+        fig_name = 'pics/' + str(freq) + '/chunks/' + str(i).zfill(3) + '.png'
+        plt.suptitle("Gamma = " + str(gamma))
         plt.savefig(fig_name)
         plt.close(fig)
-        plt.clf()
-        powers += np.sqrt(power)
-        psum = np.sum(power, axis=1)
-        psums[i,:] = psum[:]
+    vid_name = 'chunk_stack.mp4'
+    os.chdir('pics/' + str(freq) + '/chunks')
+    os.system('ffmpeg -r 10 -f image2 -s 1920x1080 -i %03d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ' + vid_name)
+    os.chdir('../../..')
+    return
 
-    plt.figure()
-    plt.suptitle('Spectrum vs. gamma')
-    levels = np.linspace(np.min(psums), np.max(psums), 30)
-    plt.contour(f, gammas, psums, levels=levels)
-    
-    plt.figure()
-    plt.suptitle('Summed pow')
-    levels = np.linspace(np.min(powers), np.max(powers), 20)
-    plt.contour(f, depths, powers.T, levels=levels)
-
-def animate_figs(freq,):
+def animate_figs(freq,chunk=None):
     vid_name = str(freq) + '.mp4'
-    os.chdir('pics/gammapows/' + str(freq))
+    if type(chunk) != type(None):
+        os.chdir('pics/gammapows/' + str(freq) + '/' + str(chunk))
+    else:
+        os.chdir('pics/gammapows/' + str(freq))
     os.system('ffmpeg -r 10 -f image2 -s 1920x1080 -i %03d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ' + vid_name)
     os.chdir('../../../')
+    if type(chunk) != type(None):
+        os.chdir('..')
 
 def limit_peaks(peak_inds, peak_heights, num_modes):
     """
@@ -166,7 +275,7 @@ def get_f_brackets(freqs, num_modes):
     right_inds[-1] = -1
     return list(zip(left_inds, right_inds))
     
-def gen_alt_figs(freq, num_modes, chunk_len=1024, threshold=1000, short=False):
+def gen_alt_figs(freq, num_modes, chunk_len=1024, threshold=1000, short=False,chunk=None):
     """ 
     For each value of gamma, pick out the peaks from the spectrum
     For each peak, I get a corresponding mode shape
@@ -186,7 +295,7 @@ def gen_alt_figs(freq, num_modes, chunk_len=1024, threshold=1000, short=False):
     ind_bracks = get_f_brackets(lim_f, num_modes) 
     for i, gamma in enumerate(gammas):
         fname = str(gamma)[:7]+'.npy'
-        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short)
+        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short,chunk=chunk)
         x = x[start_cut:end_cut,:]
         power = np.square(abs(x))
         if i == 0:
@@ -235,7 +344,7 @@ def gen_alt_figs(freq, num_modes, chunk_len=1024, threshold=1000, short=False):
     plt.contour(f, depths, powers.T, levels=levels)
     plt.show()
 
-def make_mode_movie(freq, bin_ind,short=False):
+def make_mode_movie(freq, bin_ind,short=False,chunk=None):
     names = get_names(freq)
     gammas = get_gammas_from_names(names)
     depths = np.linspace(0, 63, 64)
@@ -244,7 +353,7 @@ def make_mode_movie(freq, bin_ind,short=False):
     pows = []
     xlim = 0
     for i, gamma in enumerate(gammas):
-        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short)
+        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short,chunk=chunk)
         x = x[bin_ind,:]
         if np.max(abs(x.real)) > xlim:
             xlim = np.max(abs(x.real))
@@ -252,7 +361,7 @@ def make_mode_movie(freq, bin_ind,short=False):
             xlim = np.max(abs(x.imag))
         pows.append(np.var(x))
     for i, gamma in enumerate(gammas):
-        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short)
+        x = get_dats_for_gamma(gamma, freq, chunk_len, short=short,chunk=chunk)
         x = x[bin_ind,:]
         fig, (ax1, ax2, ax3)=plt.subplots(1,3)
         ax1.plot(x.real, depths)
@@ -276,7 +385,6 @@ def make_mode_movie(freq, bin_ind,short=False):
     os.system('rm *.png')
     os.chdir('../../../')
 
-
 def get_subplot_shape(num_modes):
     """
     Get number of rows and number of cols
@@ -287,14 +395,16 @@ def get_subplot_shape(num_modes):
     print(num_rows, num_cols)
     return num_rows, num_cols
 
-def grab_mode_ests(freq, threshold, phi,chunk_len=1024, short=False):
+def grab_mode_ests(freq, threshold, phi,chunk_len=1024, short=False,chunk=None):
     """
     Get the mode estimates from the demodulated
     data 
     """
-    names = get_names(freq, chunk_len, short=short)
+    print(short, chunk)
+    names = get_names(freq, chunk_len, short=short,chunk=chunk)
     f = get_freqs(freq)
-    gammas = get_gammas_from_names(names, short=short)
+    gammas = get_gammas_from_names(names, short=short,chunk=chunk)
+    print(gammas)
     good_freqs = []
     good_inds = []
     depths = get_depths()
@@ -316,7 +426,7 @@ def grab_mode_ests(freq, threshold, phi,chunk_len=1024, short=False):
     Perhaps due to array tilt or something like that?
     """
     for i, gamma in enumerate(gammas):
-        x = get_dats_for_gamma(gamma, freq, chunk_len,short=short)
+        x = get_dats_for_gamma(gamma, freq, chunk_len,short=short,chunk=chunk)
         power = np.square(x)
         p_sum = np.sum(power, axis=1)
         #plt.plot(f, incoh_sum)
@@ -521,17 +631,25 @@ def get_om_s():
     freqs = [49, 64, 79, 94, 109, 127]
     threshold = 500
     chunk_len=  1024
-    short=True 
+    short=False
+    freqs = [127, 385]
+    chunk=0
 
     mes= []
     for freq in freqs:
         f = get_freqs(freq)
-        phi = load_mode_shapes(freq)
+        phi, krs = load_mode_model(freq)
+        phi = phi[1:,:]
         phi = phi.real
         phi = norm_phi(phi)
-        best_gammas, best_finds, best_ests = grab_mode_ests(freq, threshold, phi, chunk_len=chunk_len,short=short)
+        best_gammas, best_finds, best_ests = grab_mode_ests(freq, threshold, phi, chunk_len=chunk_len,short=short,chunk=chunk)
         print('freq', best_gammas)
+        fig,ax = plt.subplots(1,1)
+        fig.suptitle('Peak frequency versus mode match for ' + str(freq))
+        ax.plot(krs, f[best_finds])
+        print(krs)
         plt.show()
+        
         ests = [x for x in best_ests if type(x) != type(0)]
         err = []
         for i, est in enumerate(ests):
@@ -545,7 +663,6 @@ def get_om_s():
         print(mat.shape)
         tmp =  np.linalg.pinv(mat)
         print(tmp.shape)
-
 
 def get_spec_gammas(freq):
     fs = os.listdir('./npy_files/' + str(freq) + '/spec')
@@ -605,35 +722,22 @@ def plot_mode_spec():
             plt.plot(abs(x[i,:]))
         plt.legend([str(i) for i in range(x.shape[0])])
         plt.show()
+
+def compare_mode_spec():
+    freq = 127
+    root = './npy_files/'+str(freq) + '/filt/'
+    fs = os.listdir(root)
+    for i,f in enumerate(fs):
+        x = np.load(root+f)
+        x = x.T
+        plt.subplot(511+i)
+        plt.plot(x[1,:])
+    plt.show()
+        
    
 if __name__ == '__main__': 
-        
-    #get_spec_gammas(64)
-        
-    #plot_mode_spec()
+       
     #get_om_s()
-    #gen_alt_figs(49, 6)
-    #testing_inverse(49)
-    #testing_sine_hypoth(49)
-
-        
-    #gen_figs(49, short=True)
-    #gen_figs(64, short=True)
-    animate_figs(64)
-    #gen_figs(79, short=True)
-    animate_figs(79)
-    #gen_figs(94, short=True)
-    animate_figs(94)
-    #gen_figs(109, short=True)
-    animate_figs(109)
-    #gen_figs(127, short=True)
-    animate_figs(127)
-    #freq = 49
-    #f = np.load('npy_files/' + str(freq) + '/f_grid.npy')
-    #f = f[200:500:10]
-    #print(f)
-    #for i in range(400, 500, 10):
-    #    make_mode_movie(49, i)    
-    #
-    #gen_figs(109)
-    #animate_figs(109)
+    for i in range(1): 
+        gen_figs(109, short=False,chunk=i)
+        animate_figs(109, chunk=i)
